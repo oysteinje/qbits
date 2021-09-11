e2f53b0018fc777092d13c653c28b802c3a8f907
ystrte <oyststra@gmail.com>
2021-09-11 08:03:07 +0200

    add port opening

--- bootstrap-vm.sh
+++ bootstrap-vm.sh
@@ -6,5 +6,5 @@ sudo mkdir main
 cd main
 sudo curl https://raw.githubusercontent.com/oysteinje/qbits/main/index.html -o index.html
 cd /etc/nginx/sites-enabled
-sudo curl https://raw.githubusercontent.com/oysteinje/qbits/main/vhost -o vhost
+sudo curl  -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/oysteinje/qbits/main/vhost -o vhost
 sudo service nginx restart
