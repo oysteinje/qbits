ebf87739a4dfdd4fa19d9f1b58e9b8067922abcb
ystrte <oyststra@gmail.com>
2021-09-11 07:29:03 +0200

    upd bootstrap [skip ci]

--- bootstrap-vm.sh
+++ bootstrap-vm.sh
@@ -4,7 +4,7 @@ sudo apt-get install -y nginx
 cd /var/www
 sudo mkdir main
 cd main
-curl https://raw.githubusercontent.com/oysteinje/qbits/main/index.html -o index.html
+sudo curl https://raw.githubusercontent.com/oysteinje/qbits/main/index.html -o index.html
 cd /etc/nginx/sites-enabled
-curl https://raw.githubusercontent.com/oysteinje/qbits/main/vhost -o vhost
+sudo curl https://raw.githubusercontent.com/oysteinje/qbits/main/vhost -o vhost
 sudo service nginx restart
